e63d54270bb0101b4671f3d52e94607620fe5f59
Kal222 <160258022+Kal222000@users.noreply.github.com>
2024-04-01 09:46:16 -0400

    Ejercicio 1 Prueba 3

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -4,14 +4,24 @@
 
 using namespace std;
 
-void exercise_1(string s1) {
+void exercise_1(string s1, s2) {
     getline(cin, s1);
+
     for(int i = 0; i <= s1.size(); ++i) {
         if(s1[i] == ' '){
             cout << i << endl;
         }
     }
     cout << s1.size() << endl;
+
+    getline(cin, s2);
+
+    for(int i = 0; i <= s2.size(); ++i) {
+        if(s2[i] == ' '){
+            cout << i << endl;
+        }
+    }
+    cout << s2.size() << endl;
 }
 
 void exercise_2(string s1) {
