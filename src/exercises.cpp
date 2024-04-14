6e8b0a88e4705f77006ff0ec59db9a03f6126db6
Kal222 <160258022+Kal222000@users.noreply.github.com>
2024-04-14 12:55:41 -0400

    Ejercicio 4 Prueba 9

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -24,10 +24,12 @@ void exercise_3(string s1) {
 void exercise_4(int n) {
   int auxiliar = 1;
   if(n<1){
-      cout << "El numero es negativo. Intentelo de nuevo" << n << endl;
+      cout << "El numero es negativo. Intentelo de nuevo" << endl;
+      cout << n;
   }
   else if(n>14){
-      cout << "El numero es muy grande. Intentelo de nuevo" << n << endl;
+      cout << "El numero es muy grande. Intentelo de nuevo" << endl;
+      cout << n;
   }
   for(int i = 1; i<=n; i++){
       auxiliar *= i;
@@ -46,7 +48,7 @@ int exercise_6(int n) {
 
 void exercise_7(int n) {
   double suma;
-   for(int i = 1; i <= n; ++i) {
+   for(int i = 0; i <= n; ++i) {
        double potencia = pow (-1, (i + 1) );
        double auxiliar = ( potencia / i);
        suma += auxiliar;
